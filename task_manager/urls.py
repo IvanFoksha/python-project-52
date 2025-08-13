bff31e429f0be5b3bb0904c3347cca9ccaa2c400
IvanFoksha <v.foksh6@gmail.com>
2025-08-13 12:46:05 +0200

    Create admin login

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from task_manager.views import index
+from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
+    path('admin/', admin.site.urls),
 ]
