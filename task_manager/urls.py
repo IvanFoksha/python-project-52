71e6f7c4c6856f7c85bb42818abbd351bcc8470a
IvanFoksha <v.foksh6@gmail.com>
2025-08-14 18:01:23 +0200

    Update urls: added create, update, delete for model User

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from task_manager.views import index, UserListView
+from task_manager.views import (
+    index,
+    UserListView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+)
 from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('users/', UserListView.as_view(), name='user_list')
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/create/', UserCreateView.as_view(), name='user_create'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
