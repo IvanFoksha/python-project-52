f0955e474de411cb4c95f1d7a023d99268c3b6e0
IvanFoksha <v.foksh6@gmail.com>
2025-08-14 09:12:39 +0200

    Added view for user_list

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,13 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+    context_object_name = 'users'
