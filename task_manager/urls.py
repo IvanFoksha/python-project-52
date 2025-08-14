2787b18fcbdf1db14f3eb6663288290f2309cd3a
IvanFoksha <v.foksh6@gmail.com>
2025-08-14 09:12:11 +0200

    Added user_list

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from task_manager.views import index
+from task_manager.views import index, UserListView
 from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('users/', UserListView.as_view(), name='user_list')
 ]
