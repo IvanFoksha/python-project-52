b960090ec9cfa75b45e880b93bcdb6d8a0a95a5f
IvanFoksha <v.foksh6@gmail.com>
2025-08-15 21:18:07 +0200

    Update urls: added paths for autentification users

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from task_manager.views import (
     index,
     UserListView,
     UserCreateView,
     UserUpdateView,
     UserDeleteView,
+    CustomLoginView,
 )
 from django.contrib import admin
 
@@ -15,4 +17,12 @@ urlpatterns = [
     path('users/create/', UserCreateView.as_view(), name='user_create'),
     path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(
+            next_page='index',
+            template_name='logout.html'
+        ),
+        name='logout'),
 ]
