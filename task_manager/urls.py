5015a7fff2f5a0df98788a8c96e68e2a6e61007e
IvanFoksha <v.foksh6@gmail.com>
2025-08-18 16:47:31 +0200

    Update paths for status in urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -8,6 +8,10 @@ from task_manager.views import (
     UserUpdateView,
     UserDeleteView,
     CustomLoginView,
+    StatusListView,
+    StatusCreateView,
+    StatusUpdateView,
+    StatusDeleteView,
 )
 from django.contrib import admin
 
@@ -17,8 +21,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/create/', UserCreateView.as_view(), name='user_create'),
-    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
-    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+    path(
+        'users/<int:pk>/update/',
+        UserUpdateView.as_view(),
+        name='user_update'
+    ),
+    path(
+        'users/<int:pk>/delete/',
+        UserDeleteView.as_view(),
+        name='user_delete'
+    ),
     path('login/', CustomLoginView.as_view(), name='login'),
     path(
         'logout/',
@@ -27,4 +39,16 @@ urlpatterns = [
             template_name='logout.html'
         ),
         name='logout'),
+    path('statuses/', StatusListView.as_view(), name='status_list'),
+    path('statuses/create', StatusCreateView.as_view(), name='status_create'),
+    path(
+        'statuses/<int:pk>/update/',
+        StatusUpdateView.as_view(),
+        name='status_update'
+    ),
+    path(
+        'statuses/<int:pk>/delete/',
+        StatusDeleteView.as_view(),
+        name='status_delete'
+    ),
 ]
