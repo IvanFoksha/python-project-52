1c4670932060b704e7ddebcd0e601c47629727eb
IvanFoksha <v.foksh6@gmail.com>
2025-08-18 18:31:31 +0200

    Update settings for migrations

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -115,6 +115,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
