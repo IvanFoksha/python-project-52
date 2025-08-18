54c84a83943165e2cf40b1d9a838f81140366ec5
IvanFoksha <v.foksh6@gmail.com>
2025-08-18 16:46:48 +0200

    Update views: added CRUD for status model

--- task_manager/views.py
+++ task_manager/views.py
@@ -11,12 +11,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import (
     login,
-    authenticate,
     update_session_auth_hash,
 )
 from django.views import View
 from django.contrib import messages
 from .forms import CustomUserCreationForm, UserChangeForm
+from .models import Status
 
 
 def index(request):
@@ -76,7 +76,7 @@ class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def handle_no_permission(self):
         messages.error(
-            self. request,
+            self.request,
             'У вас нет прав для редактированя этого профиля.'
         )
         return redirect('user_list')
@@ -124,3 +124,100 @@ class CustomLoginView(View):
             return redirect('index')
         messages.error(request, 'Неверные данные или ошибка входа.')
         return render(request, 'index.html', {'form': form})
+
+
+class StatusListView(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = 'status_list.html'
+    context_object_name = 'statuses'
+
+    def get_queryset(self):
+        return Status.objects.all()
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    template_name = 'status_create.html'
+    success_url = reverse_lazy('status_list')
+    fields = ['name']
+
+    def form_valid(self, form):
+        status = form.save()
+        messages.success(
+            self.request,
+            f'Статус "{status.name}" успешно создан!'
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(
+            self.request,
+            'Ошибка создания статуса. Проверьте данные.'
+        )
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
+    model = Status
+    template_name = 'status_update.html'
+    success_url = reverse_lazy('status_list')
+    fields = ['name']
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and self.request.user == self.get_object()
+        )
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request,
+            'У вас нет прав для редактированя этого статуса.'
+        )
+        return redirect('status_list')
+
+    def form_valid(self, form):
+        status = form.save()
+        messages.success(
+            self.request,
+            f'Статус "{status.name}" успешно обновлен!'
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(
+            self.request,
+            'Ошибка обновления статуса. Проверьте данные.'
+        )
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
+    model = Status
+    template_name = 'status_delete.html'
+    success_url = reverse_lazy('status_list')
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and self.request.user == self.get_object()
+        )
+
+    def handle_no_permission(self):
+        messages.error(
+            self. request,
+            'У вас нет прав для удаления этого профиля.'
+        )
+        return redirect('status_list')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.has_related_tasks():
+            messages.error(request, 'Нельзя удалить статус, связанный с задачами.')
+            return self.render_to_response(self.get_context_data())
+        self.object.delete()
+        messages.success(request, f'Статус "{self.object.name}" успешно удален!')
+        return redirect(self.get_success_url())
