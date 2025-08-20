d9143ddb1d051d83952b835ba3dadf95cfb55400
IvanFoksha <v.foksh6@gmail.com>
2025-08-20 20:35:41 +0200

    Update models: added Task model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Status(models.Model):
@@ -13,4 +14,36 @@ class Status(models.Model):
         return self.name
 
     def has_related_tasks(self):
-        return False
+        return Task.objects.filter(status=self).exists()
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200, help_text='Название задачи')
+    description = models.TextField(blank=True, help_text='Описание задачи')
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        help_text='Статус задачи'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='authored_tasks',
+        help_text='Автор задачи'
+    )
+    assignee = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='assigned_tasks',
+        help_text='Исполнитель задачи'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
