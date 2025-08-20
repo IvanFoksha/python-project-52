4d0807654df4cb028b3c68dd3e8716cc9cc6a74b
IvanFoksha <v.foksh6@gmail.com>
2025-08-20 20:37:10 +0200

    Update urls: added paths for Task model

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -12,6 +12,11 @@ from task_manager.views import (
     StatusCreateView,
     StatusUpdateView,
     StatusDeleteView,
+    TaskListView,
+    TaskCreateView,
+    TaskDetailView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 from django.contrib import admin
 
@@ -50,5 +55,22 @@ urlpatterns = [
         'statuses/<int:pk>/delete/',
         StatusDeleteView.as_view(),
         name='status_delete'
+    ),
+    path('tasks/', TaskListView.as_view(), name='task_list'),
+    path('tasks/create', TaskCreateView.as_view(), name='task_create'),
+    path(
+        'tasks/<int:pk>/update/',
+        TaskUpdateView.as_view(),
+        name='task_update'
+    ),
+    path(
+        'tasks/<int:pk>/delete/',
+        TaskDeleteView.as_view(),
+        name='task_delete'
+    ),
+    path(
+        'tasks/<int:pk>',
+        TaskDetailView.as_view(),
+        name='task_detail'
     ),
 ]
