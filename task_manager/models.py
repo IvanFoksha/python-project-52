0117758fa33c697ada7e50f851a3a95fdd84e662
IvanFoksha <v.foksh6@gmail.com>
2025-08-20 18:07:07 +0200

    Update models: added created_atin Status

--- task_manager/models.py
+++ task_manager/models.py
@@ -7,6 +7,7 @@ class Status(models.Model):
         unique=True,
         help_text='Название статуса'
     )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
