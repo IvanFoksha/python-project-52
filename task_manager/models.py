688f2a5db80768a14e0c653a7528bfd0cac33339
IvanFoksha <v.foksh6@gmail.com>
2025-08-21 09:56:22 +0200

    Update models: added Label model and ManyToMany logic with Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -40,6 +40,11 @@ class Task(models.Model):
         help_text='Исполнитель задачи'
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    labels = models.ManyToManyField(
+        'Label',
+        blank=True,
+        help_text='Метки задачи'
+    )
 
     def __str__(self):
         return self.name
@@ -47,3 +52,18 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=100, help_text='Название метки')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def has_related_tasks(self):
+        return self.task_set.exists()
+
+    class Meta:
+        verbose_name = 'Метка'
+        verbose_name_plural = 'Метки'
