47cca35a0d813f5012bf42567947984cdb577566
IvanFoksha <v.foksh6@gmail.com>
2025-09-02 13:16:37 +0200

    Delete checking output

--- task_manager/tests/tests_filters.py
+++ task_manager/tests/tests_filters.py
@@ -55,7 +55,6 @@ class FilterCRUDTests(TestCase):
         self.assertEqual(len(tasks), 1)
         self.assertEqual(tasks[0], self.task1)
         self.assertIn(self.label, tasks[0].labels.all())
-        print(f"Filtered tasks (labels): {tasks}")
 
     def test_task_filter_by_own_tasks(self):
         self.client.login(username='testuser1', password='TestPass123')
@@ -64,15 +63,13 @@ class FilterCRUDTests(TestCase):
         tasks = response.context['tasks']
         self.assertEqual(len(tasks), 1)
         self.assertEqual(tasks[0], self.task1)
-        print(f"Filtered tasks (own_tasks): {tasks}")
 
     def test_task_filter_no_params(self):
         self.client.login(username='testuser1', password='TestPass123')
         response = self.client.get(self.task_list_url)
         self.assertEqual(response.status_code, 200)
         tasks = response.context['tasks']
-        self.assertEqual(len(tasks), 2)  # Ожидаем все задачи без фильтров
-        print(f"Tasks with no params: {tasks}")
+        self.assertEqual(len(tasks), 2)
 
     def test_task_filter_unauthorized(self):
         response = self.client.get(self.task_list_url, {'status': str(self.status.pk)})
