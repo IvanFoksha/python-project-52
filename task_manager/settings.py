57809c6fc7764a9a62d9a4612b72e70df6d6f697
IvanFoksha <v.foksh6@gmail.com>
2025-09-20 13:33:32 +0200

    Update settings - added correct DB for building and CI for git

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,43 +75,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-if 'RENDER' in os.environ:  # Для prod на Render
+# PostgreSQL для Render, SQLite для локальной разработки
+if 'RENDER' in os.environ:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL'),
-            conn_max_age=600,
-            conn_health_checks=True,
+        'default': (
+            dj_database_url.config(
+                default=os.getenv('DATABASE_URL'),
+                conn_max_age=600,
+                conn_health_checks=True,
+            )
         )
     }
-else:  # Для local dev с Docker PG
+else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'mydb',
-            'USER': 'myuser',
-            'PASSWORD': 'mypassword',
-            'HOST': 'localhost',
-            'PORT': '5432',
-          }
-      }
-
-# PostgreSQL для Render, SQLite для локальной разработки
-# DATABASE_URL = os.getenv('DATABASE_URL')
-# if DATABASE_URL:
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default=DATABASE_URL,
-#             conn_max_age=600,
-#             conn_health_checks=True,
-#         )
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # SQLite для локальной разработки
 
