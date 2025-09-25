fa8912d7aee69caadd5f0f562ec5bec1e1912d41
IvanFoksha <v.foksh6@gmail.com>
2025-09-25 16:13:04 +0200

    Update migrate user and remigrate project

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     class Meta:
-        db_table = 'users'
         verbose_name = 'Пользователя'
         verbose_name_plural = 'Пользователи'
 
